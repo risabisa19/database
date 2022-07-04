797f0f26b493c5a00a5633c749a2dc4ebe052e33
risabisa19 <91229796+risabisa19@users.noreply.github.com>
2022-07-04 09:57:33 +0700

    hh
    update

---
+++ index.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const app = express()
+const  port = 5000
+
+//connec url
+const url='mongodb+srv://risa:risa@cluster0.npdjknu.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(url);
+
+//database name
+const dbName ='mantappu';
+
+app.get('/', async(req,res) => {
+    await client.connect();
+    console.log('Connected Sucsses');
+    const db = client.db(dbName);
+    const collection = db.collection('mantap');
+    const findResult = await collection.find({}).toArray();
+    res.status(200).json({Datajson : findResult})
+})
+
+app.listen(port, () => {
+    console.log(`Example listen on port ${port}`)
+})
